01fadaa5465cd9b0535e6bff0dff30dc3be94bf6
tawarimayur10 <tawarimayur10@gmail.com>
2020-10-06 07:24:40 +0530

    Brewery Initial Development

---
+++ Documents/SpringBootWorkspace/msscbrewery/src/main/java/guru/springframework/msscbrewery/MsscbreweryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscbreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscbreweryApplication.class, args);
+	}
+
+}
